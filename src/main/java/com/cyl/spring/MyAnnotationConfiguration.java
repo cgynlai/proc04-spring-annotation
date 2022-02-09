279dddcfa92e8c04cfd854240a5b9c2fd91c5b9a
cgynlai <cgynlai@gmail.com>
2022-02-09 22:54:53 +0800

    add annotaion

--- src/main/java/com/cyl/spring/MyAnnotationConfiguration.java
+++ src/main/java/com/cyl/spring/MyAnnotationConfiguration.java
@@ -9,6 +9,7 @@ import com.cyl.spring.entity.Employee;
 @Configuration
 public class MyAnnotationConfiguration {
     //Bean注解相当于做了下面XML标签的配置，把方法的返回值放入IOC容器
+	//Bean注解相当于做了下面XML标签的配置，把方法的返回值放入IOC容器
 	//<bean id="" class="com.cyl.spring.entity.Employee" />
 	@Bean
 	public Employee getEmployee() {
